9d523329f01d5a2c5029238e323dbb10e5674a00
unknown <mrgwduncan@gmail.com>
2020-01-24 09:50:46 +1300

    index done

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,25 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  // db.getTrainers()
-  //     .then (trainers => {
-  //       db.getpokemon()
-  //       .then(type => {
-  //           res.render('index', {trainers: trainers, pokemon: pokemon })
-  //       })
+  db.getTrainers()
+      .then (trainers => {
+        db.getpokemon()
+        .then(pokemon => {
+            res.render('index', {trainers: trainers, pokemon: pokemon })
+        })
         
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send('DATABASE ERROR: ' + err.message)
-  //   })
-  res.render('index')
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.post ('/', (req, res) => {
+  if(req.body.trainer !== 'None'){
+    res.redirect('/trainer/'+req.body.trainer)
+} else {
+    res.redirect('/pokemon/'+req.body.pokemon)
+}
 })
 
 module.exports = router
