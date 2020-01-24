e8cd485abe1e55bda97e0ba7d1d826c3c63720af
Student <student@eda-bc-02.localdomain>
2020-01-24 15:40:32 +1300

    seb update

--- tests/seeds/0-clean.js
+++ tests/seeds/0-clean.js
@@ -2,8 +2,9 @@ exports.seed = function (knex, Promise) {
   const empty = table =>
     () => knex(table).del()
 
-  return empty('users')()
+  return empty('trainers')()
   // Chain calls to empty in
   // order as required, e.g.:
   // .then(empty('profiles'))
 }
+
